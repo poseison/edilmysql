f45c6d02435f8e9884136e1f34e4f2f756cf59ae
poseison <chlaliotis@gmail.com>
2014-08-09 21:21:58 +0300

    fdfd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,10 @@ Mygasmeter::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  cconfig.serve_static_assets = false
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -28,9 +31,9 @@ Mygasmeter::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-
  
   config.assets.precompile << /\A(?!active_admin).*\.(js|scss|coffee)\z/
+  config.assets.precompile << %w( *.coffee )
   # Generate digests for assets URLs.
   config.assets.digest = true
 
